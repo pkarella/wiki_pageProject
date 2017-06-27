ed31412cc04966fc540681e1a28dbcff2f3d7418
peter <pkarella27@yahoo.com>
2017-06-26 18:02:17 -0700

    refactoring

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,38 @@ class CategoriesController < ApplicationController
     @categorie= Categorie.find(params[:id])
     render :show
   end
+
+  def new
+   @categorie = Categorie.new
+   render :new
+ end
+
+ def create
+    @categorie = Categorie.new(categorie_params)
+    if @categorie.save
+      redirect_to  categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+      @categorie = Categorie.find(params[:id])
+      render :edit
+    end
+
+ def update
+   @categorie= List.find(params[:id])
+   if @categorie.update(categorie_params)
+     redirect_to categories_path
+   else
+     render :edit
+   end
+ end
+
+private
+  def categorie_params
+    params.require(:categorie).permit(:title)
+  end
+
 end
